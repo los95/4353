8275ca97861febba481e42252f402a66d0803497
Orion-ctrl <51823686+Orion-ctrl@users.noreply.github.com>
2022-03-13 23:11:45 -0500

    Update main.go

--- main.go
+++ main.go
@@ -2,150 +2,86 @@ package main
 
 import (
 	"errors"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"testing"
 )
 
-type FuelQuote struct {
-	Username string `json: "username"`
-	Gallons  int    `json: "gallons"`
+func TestGetUsers(t *testing.T) {
+	actualResult := getUsers()
+	expectedResult := []User{
+		{Username: "test user", FirstName: "This", LastName: "Mf", Residence: Address{"123 Cougar Drive", "Houston", "Texas", "77070"}},
+	}
+
+	for i, user := range actualResult {
+		if expectedResult[i] != user {
+			t.Errorf("Arrays do not match")
+		}
+	}
 }
 
-type Address struct {
-	Street  string `json: "street"`
-	City    string `json: "city"`
-	State   string `json: "state"`
-	Zipcode string `json: "zipcode"`
-}
-
-type User struct {
-	Username  string  `json: "username"`
-	FirstName string  `json: "firstName"`
-	LastName  string  `json: "lastName"`
-	Residence Address `json: "address"`
-}
-
-type Credentials struct {
-	Username string `json: "username"`
-	Password string `json: "password"`
-}
-
-var userCredentials = []Credentials{
-	{Username: "test user", Password: "test password"},
-}
-
-var users = []User{
-	{Username: "test user", FirstName: "This", LastName: "Mf", Residence: Address{"123 Cougar Drive", "Houston", "Texas", "77070"}},
-}
-
-var fuelQuoteHistory = []FuelQuote{
-	{Username: "test user", Gallons: 2},
-}
-
-func getUsers() []User {
-	return users
-}
-
-func registerNewUser(newUser Credentials) {
-	userCredentials = append(userCredentials, newUser)
-	users = append(users, User{newUser.Username, "", "", Address{}})
-}
-
-func register(c *gin.Context) {
-	var newUser Credentials
-
-	if err := c.BindJSON(&newUser); err != nil {
-		return
+func TestRegistration(t *testing.T) {
+	newUser := Credentials{
+		Username: "testUserName",
+		Password: "testPassword",
 	}
 
 	registerNewUser(newUser)
-	c.IndentedJSON(http.StatusCreated, newUser)
-}
 
-func getUser(credentials Credentials) (*User, error) {
-	for i, user := range userCredentials {
-		if user.Username == credentials.Username {
-			if user.Password == credentials.Password {
-				return &users[i], nil
-			} else {
-				return nil, errors.New("incorrect password")
-			}
-		}
-	}
-
-	return nil, errors.New("User not found")
-}
-
-func login(c *gin.Context) {
-	var credentials Credentials
-
-	if err := c.BindJSON(&credentials); err != nil {
-		return
-	}
-
-	user, err := getUser(credentials)
-
-	if err != nil {
-		c.IndentedJSON(http.StatusAccepted, user)
-	} else {
-		c.IndentedJSON(http.StatusBadRequest, err)
+	if len(users) != 2 && len(userCredentials) != 2 {
+		t.Errorf("User Length and/or Credential Lengths do not match")
 	}
 }
 
-func editProfile(username string, address Address) error {
-	for i, user := range users {
-		if user.Username == username {
-			users[i].Residence = address
-			return nil
-		}
+func TestRegistrationDuplicateUser(t *testing.T) {
+	newUser := Credentials{
+		Username: "test user",
+		Password: "testPassword",
 	}
 
-	return errors.New("unable to find user")
-}
-
-func profile(c *gin.Context) {
-	type tempAddress struct {
-		username    string  `json: username`
-		profileInfo Address `json: profileInfo`
-	}
-
-	var userInfo tempAddress
-
-	c.BindJSON(&userInfo)
-
-	if err := editProfile(userInfo.username, userInfo.profileInfo); err != nil {
-		c.IndentedJSON(http.StatusAccepted, userInfo)
-	} else {
-		c.IndentedJSON(http.StatusBadRequest, err)
+	if !userExists(newUser.Username) {
+		t.Errorf("User already exists but still registered")
 	}
 }
 
-func getHistory(username string) (int, error) {
-	for _, user := range fuelQuoteHistory {
-		if user.Username == username {
-			return user.Gallons, nil
-		}
-	}
+func TestLoginWrongUser(t *testing.T) {
+	_, err := getUser(Credentials{"wrong username", "some password"})
 
-	return 0, errors.New("no history found")
-}
+	fmt.Println(err)
 
-func history(c *gin.Context) {
-	username := c.Param("username")
-
-	if history, err := getHistory(username); err != nil {
-		c.IndentedJSON(http.StatusAccepted, history)
-	} else {
-		c.IndentedJSON(http.StatusBadRequest, err)
+	if err == errors.New("User not found") {
+		t.Errorf("Still logged in ")
 	}
 }
 
-func main() {
-	router := gin.Default()
-	router.POST("/register", register)
-	router.POST("/login", login)
-	router.POST("/profile", profile)
-	router.GET("/getHistory", history)
-	router.Run("localhost:8080")
+func TestLoginWrongPassword(t *testing.T) {
+	_, err := getUser(Credentials{"test user", "some password"})
+
+	if err == errors.New("incorrect password") {
+		t.Errorf("Still logged in ")
+	}
+}
+
+func TestLoginValid(t *testing.T) {
+	result, _ := getUser(Credentials{"test user", "test password"})
+	expectedResult := User{"test user", "This", "Mf", Address{"123 Cougar Drive", "Houston", "Texas", "77070"}}
+
+	if *result != expectedResult {
+		t.Errorf("Did not log in")
+	}
+}
+
+func TestProfileEditingValid(t *testing.T) {
+	result := editProfile("test user", Address{"", "", "", ""})
+
+	if result != nil {
+		t.Errorf("Did not edit profile")
+	}
+}
+
+func TestProfileEditingInvalid(t *testing.T) {
+	result := editProfile("wrong username", Address{"", "", "", ""})
+
+	if result == errors.New("unable to find user") {
+		t.Errorf("Somehow found user")
+	}
 }
