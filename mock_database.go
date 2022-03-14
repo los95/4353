7ebb397bee9a0ffe568fad79cddc9bf8f4ecff4d
Los <cavalencia@protonmail.com>
2022-03-14 04:19:34 +0000

    Add files via upload

---
+++ mock_database.go
@@ -0,0 +1,13 @@
+package main
+
+var userCredentials = []Credentials{
+	{Username: "test user", Password: "test password"},
+}
+
+var users = []User{
+	{Username: "test user", FirstName: "This", LastName: "Mf", Residence: Address{"123 Cougar Drive", "Houston", "Texas", "77070"}},
+}
+
+var fuelQuoteHistory = []FuelQuote{
+	{Username: "test user", Gallons: 2},
+}
